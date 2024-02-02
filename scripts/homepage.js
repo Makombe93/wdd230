8dac4542e9f9cce9e3f4864a983a0374e0eee87a
Makombe93 <mak20002@byui.edu>
2024-02-02 17:17:13 +0200

    Commit

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuBtn = document.querySelector('.menu');
     const navList = document.querySelector('.navmenu');
+    const darkModeBtn = document.getElementById('dark-mode')
 
     menuBtn.addEventListener('click', function () {
         navList.classList.toggle('active');
         menuBtn.classList.toggle('active');
     });
 
-    document.getElementById('dark-mode-btn').addEventListener('click', function () {
-        document.body.classList.toggle('dark-mode-btn');
+    darkModeBtn.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
     });
 });
