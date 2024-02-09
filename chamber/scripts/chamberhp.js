71028124e919b2787fe96a75bce4ef2d050ae506
Makombe93 <mak20002@byui.edu>
2024-02-09 15:12:57 +0200

    Commit

--- chamber/scripts/chamberhp.js
+++ chamber/scripts/chamberhp.js
@@ -7,3 +7,28 @@ document.addEventListener('DOMContentLoaded', function () {
         hamMenu.classList.toggle('active');
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var visitMessage = document.getElementById("visit-message");
+    var lastVisit = localStorage.getItem("lastVisit");
+
+    if (lastVisit) {
+        var currentDate = new Date();
+        var millisecondsDiff = currentDate - lastVisit;
+        var daysDiff = Math.floor(millisecondsDiff / (1000 * 60 * 60 * 24));
+
+        if (daysDiff === 0) {
+            visitMessage.textContent = "Back so soon! Awesome!";
+        } else if (daysDiff === 1) {
+            visitMessage.textContent = "You last visited 1 day ago.";
+        } else {
+            visitMessage.textContent = "You last visited " + daysDiff + " days ago.";
+        }
+    } else {
+        visitMessage.textContent = "Welcome! Let us know if you have any questions.";
+    }
+
+    localStorage.setItem("lastVisit", Date.now());
+});
+
